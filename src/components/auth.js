81e29f35a0c2698d9e10355eee20319959744181
ameliatjy <e0425671@u.nus.edu>
2020-05-26 18:17:07 +0800

    Login page backend

--- src/components/auth.js
+++ src/components/auth.js
@@ -5,12 +5,19 @@ export const logoutUser = () => {
     firebase.auth().signOut();
 };
 
-export const signUpUser = async ({ name, email, password }) => {
+export const signUpUser = async ({ name, email, password, confirmPassword }) => {
     if (name.length <= 0) { return { error: "Name cannot be empty." }
     } else if (email.length <= 0) { return { error: "Email cannot be empty." }
     } else if (password.length <= 0) { return { error: "Password cannot be empty." }
+    } else if (password.length < 6) { return { error: "Password cannot be less than 6 characters."}
+    } else if (confirmPassword.length <= 0) { return { error: "Please retype your password for confirmation."}
+    } else if (confirmPassword != password) { return { error: "Passwords mismatch."}
     } else {
         return firebase.auth().createUserWithEmailAndPassword(email, password).then(function (user) {
+            firebase.database().ref('users/' + name).set({
+                name: name,
+                email: email
+            })
             var user = firebase.auth().currentUser;
             // console.log(user);
             return user;
@@ -40,28 +47,32 @@ export const signUpUser = async ({ name, email, password }) => {
 }
 
 export const loginUser = async ({ email, password }) => {
-    try {
-        await firebase.auth().signInWithEmailAndPassword(email, password);
-        return {};
-    } catch (error) {
-        switch (error.code) {
-            case "auth/invalid-email":
-                return {
-                    error: "Invalid email address format."
-                };
-            case "auth/user-not-found":
-            case "auth/wrong-password":
-                return {
-                    error: "Invalid email address or password."
-                };
-            case "auth/too-many-requests":
-                return {
-                    error: "Too many request. Try again in a minute."
-                };
-            default:
-                return {
-                    error: "Check your internet connection."
-                };
-        }
+    if (email.length <= 0) { return { error: "Email cannot be empty." }
+    } else if (password.length <= 0) { return { error: "Password cannot be empty." }
+    } else {
+        return firebase.auth().signInWithEmailAndPassword(email, password).then(function (user) {
+            //var user = firebase.auth().currentUser;
+            return user;
+        }, function (error) {
+            switch (error.code) {
+                case "auth/invalid-email":
+                    return {
+                        error: "Invalid email address format."
+                    };
+                case "auth/user-not-found":
+                case "auth/wrong-password":
+                    return {
+                        error: "Invalid email address or password."
+                    };
+                case "auth/too-many-requests":
+                    return {
+                        error: "Too many request. Try again in a minute."
+                    };
+                default:
+                    return {
+                        error: "Check your internet connection."
+                    };
+            }
+        })
     }
 };
