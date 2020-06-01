752db593b94ffed011537bb95bd6ea4040962fe3
lohwenyu <e0421622@u.nus.edu>
2020-06-01 10:07:39 +0800

    dashboard almost fully done

--- src/announcements/Announcements.js
+++ src/announcements/Announcements.js
@@ -1,90 +1,184 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-} from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import { StyleSheet, Alert, View, Text, Keyboard } from 'react-native';
+import { ScrollView, TouchableOpacity, TouchableWithoutFeedback } from 'react-native-gesture-handler';
+import { GiftedChat, Bubble, Send } from 'react-native-gifted-chat';
+import IconMCI from 'react-native-vector-icons/MaterialCommunityIcons';
+
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import { SafeAreaView } from 'react-navigation';
 
 export default class Announcements extends Component {
+
     state = {
-        announcements:
-        [
-            {
-                id: 0,
-                title: 'Hall Application for AY2021',
-                message:
-                'Dear Shearites, \n' +
-                'The offer for AY2021 is already out on the UHMS portal and please' +
-                'remember to pay the acceptance fee by 20 April.\nPlease note that ' +
-                'if you do not pay by 20 April, your slot will be forfeited.'
-            },
-            {
-                id: 1,
-                title:  'Hall Welfare Pack',
-                message:
-                'Hi Shearites! \n' +
-                'You should have received your hall welfare pack at your room.' +
-                'From the SCRC, RLT and HWAC, we wish you all the best for the ' +
-                'rest of the semester! \nStay safe and study hard!'
-            },
-            {
-                id: 2,
-                title:  'Points Release',
-                message:
-                'blah \nblah \nblah \nblah'
-            },
-            {
-                id: 3,
-                title: 'Crepe Live Station',
-                message:
-                'blah \nblah \nblah \nblah'
-            },
-            {
-                id: 4,
-                title: 'Themed Dinner',
-                message: 
-                'blah \nblah \nblah \nblah'
-            },
-            {
-                id: 5,
-                title: 'Remember to update your temperature records!',
-                message:
-                'blah \nblah \nblah \nblah'
-            }
-        ]
+        messages: [],
+    };
+
+    // can be changed later to check access different eg based on cca
+    checkAccess = () => {
+        var curruser = firebase.auth().currentUser
+        var matric = curruser.displayName
+        var curraccess
+        firebase.database().ref('users/'+ matric).on('value', function(snapshot) {
+            // curremail = snapshot.val().email;
+            curraccess = snapshot.val().accessToAnnouncements;
+        })
+        return curraccess
     }
-  render() {
-    return (
-        <View style={styles.container}>
-            <ScrollView>
-                {
-                    this.state.announcements.map((item, index) => (
-                        <View key={item.id} style={styles.item}>
-                            <Text style={{fontWeight: 'bold'}}>{item.title}</Text>
-                            <Text>{item.message}</Text>
-                        </View>
-                    ))
-                }
-            </ScrollView>
-        </View>
-    );
-  }
+
+    send = (messages) => {
+        messages.forEach(item => {
+            const message = {
+                text: item.text,
+                timestamp: firebase.database.ServerValue.TIMESTAMP,
+                user: item.user
+            };
+            return firebase.database().ref('messages').push(message);
+        })
+    }
+
+    parse = (message) => {
+        const { user, text, timestamp } = message.val()
+        const { key: _id } = message
+        const createdAt = new Date(timestamp)
+
+        return {
+            _id, 
+            createdAt,
+            text,
+            user
+        }
+    }
+
+    get = (callback) => {
+        return firebase.database().ref('messages').on('child_added', snapshot => callback(this.parse(snapshot)))
+    }
+
+    off = () => {
+        return firebase.database().ref('messages').off()
+    }
+
+    get user() {
+        var curruser = firebase.auth().currentUser
+        var matric = curruser.displayName
+        var currname
+        // curremail
+        firebase.database().ref('users/'+ matric).on('value', function(snapshot) {
+            // curremail = snapshot.val().email;
+            currname = snapshot.val().name;
+        })
+        return {
+            _id: curruser.uid,
+            name: currname,
+            // email: curremail,
+            // add in avatar
+        };
+    }
+
+    componentDidMount() {
+        this.get(message =>
+            this.setState(previousState => ({
+                messages: GiftedChat.append(previousState.messages, message),
+            }))
+        );
+    }
+
+    componentWillUnmount() {
+        this.off();
+    }
+
+    renderBubble = (props) => {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right : {
+                        backgroundColor: '#ffae50'
+                    },
+                    left : {
+                        backgroundColor: '#616161'
+                    }
+                }}
+                textProps={{
+                    style: {
+                        color: props.position === 'left' ? '#fff' : '#000',  
+                    },
+                }}
+                textStyle={{
+                    right : {
+                        // color: '#fff',
+                        fontSize: 17
+                    },
+                    left : {
+                        // color: '#fff',
+                        fontSize: 17
+                    }
+                }}
+            />
+        );
+    }
+
+    renderSend = (props) => {
+        return (
+            <Send {...props}>
+                {/* <View style={styles.sendingCon}> */}
+                <View>
+                    <IconMCI name='send-circle' size={45} color='#ff7d1d' />
+                </View>
+            </Send>
+        );
+    }
+
+    scrollToBottomComponent = () => {
+        return (
+            <View>
+                <IconMCI name='chevron-double-down' size={36} color='#ff7d1d' />
+            </View>
+        );
+    }
+
+    render() {
+        return (
+            // <SafeAreaView style={{flex: 1}}>
+                <GiftedChat 
+                    messages={this.state.messages}
+                    onSend={this.send}
+                    user={this.user}
+                    bottomOffset={0}
+                    placeholder={'To all Shearites...'}
+                    renderInputToolbar={! this.checkAccess() ? () => null : undefined}
+                    renderComposer={! this.checkAccess() ? () => null : undefined}
+                    minInputToolbarHeight={! this.checkAccess() ? 0 : undefined}
+                    renderUsernameOnMessage={true}
+                    scrollToBottom={true}
+                    scrollToBottomComponent={this.scrollToBottomComponent}
+                    keyboardShouldPersistTaps={'never'}
+                    renderBubble={this.renderBubble}
+                    renderSend={this.renderSend}
+                />
+            // </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingTop: 10,
-    paddingBottom: 10
-  },
-  item: {
-      backgroundColor: '#ECECEC',
-      marginTop: 4,
-      padding: 10,
-      marginLeft: 15,
-      marginRight: 15,
-      borderRadius: 15,
-  }
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        paddingTop: 10,
+        paddingBottom: 10
+    },
+    item: {
+        backgroundColor: '#ECECEC',
+        marginTop: 4,
+        padding: 10,
+        marginLeft: 15,
+        marginRight: 15,
+        borderRadius: 15,
+    },
+    sendingCon: {
+        marginLeft: 20,
+        justifyContent: 'center',
+        alignSelf: 'center'
+    }
 });
