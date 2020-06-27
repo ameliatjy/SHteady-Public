f7312578329c7090d147c96f0b4eddd093d3319c
ameliatjy <e0425671@u.nus.edu>
2020-06-27 17:19:46 +0800

    jest testing

--- App.test.js
+++ App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderer from 'react-test-renderer'
+import '@expo/vector-icons';
 
 import App from './App';
 
---
+++ src/dashboard/__test__/Dashboard.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import renderer from 'react-test-renderer'
+
+import Dashboard from '../Dashboard';
+
+//import firebase from 'firebase/app';
+import 'firebase/database'
+
+const firebase = require('firebase');
+//const admin = require('firebase-admin');
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyB1oyaDAneBvtqpJJqYN_o13jWDExpRDq0",
+    authDomain: "shteady-b81ed.firebaseapp.com",
+    databaseURL: "https://shteady-b81ed.firebaseio.com",
+    projectId: "shteady-b81ed",
+    storageBucket: "gs://shteady-b81ed.appspot.com",
+    messagingSenderId: "749591564782",
+    appId: "1:749591564782:web:73f597ecbcf1edd21dfeff",
+    measurementId: "G-CBS54V1147"
+};
+firebase.initializeApp(firebaseConfig);
+
+jest.useFakeTimers();
+
+test('Renders snapshot as expected', () => {
+    const tree = renderer.create(<Dashboard />).toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+// describe('<Dashboard />', () => {
+//       it('fake test', () => {
+//           expect(true).toBeTruthy();
+//       })
+//     });
