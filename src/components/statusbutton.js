0343f57a411a4f546e64035acfdc630e359ba554
ameliatjy <e0425671@u.nus.edu>
2020-06-30 19:30:04 +0800

    tried to change to class

--- src/components/statusbutton.js
+++ src/components/statusbutton.js
@@ -7,11 +7,32 @@ import {
 
 import Circle from 'react-native-vector-icons/FontAwesome'
 
+// export default class StatusButton extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+
+//     render() {
+//         return (
+//             <View style={styles.container}>
+//                 <Circle name="circle" size={38}
+//                     style={{
+//                         opacity: 0.8,
+//                         color: this.props === 'yo hmu i am in' ? '#39ff14'
+//                             : this.props === 'i am out of hall' ? '#ff0000'
+//                                 : this.props === 'busy... do not find me' ? '#fed000'
+//                                     : '#b9beb9' // accounts that havent been set up
+//                     }} />
+//             </View>
+//         )
+//     }
+// }
+
 export const StatusButton = ({ type }) => (
     <View style={styles.container}>
         <Circle name="circle" size={38}
             style={{
-                opacity:0.8,
+                opacity: 0.8,
                 color: type === 'yo hmu i am in' ? '#39ff14'
                     : type === 'i am out of hall' ? '#ff0000'
                         : type === 'busy... do not find me' ? '#fed000'
